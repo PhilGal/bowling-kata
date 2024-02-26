261e0faed88d45ef97d208e770ec0561f3f95c11
Philipp Galichkin <phil.gal@outlook.com>
2024-02-26 18:08:11 +0100

    removed unnecessary tests

--- src/test/java/io/pgl/ScoringTest.java
+++ src/test/java/io/pgl/ScoringTest.java
@@ -15,39 +15,6 @@ class ScoringTest {
   }
 
   @Test
-  void pointsAfterStrike() {
-    // Frame 1 -> X
-    assertEquals(Score.Points.pending(), player.bowl(Roll.STRIKE));
-    // Frame 2 - 9
-    player.bowl(new Roll(9));
-    assertEquals(Score.Points.pending(), player.getPoints());
-    player.bowl(new Roll(0));
-    assertEquals(Score.Points.of(28), player.getPoints());
-    // Frame 1 -> 19, Frame 2 -> 19 + 9 = 28
-  }
-
-  @Test
-  void pointsAfterSpare() {
-    // Frame 1 -> /
-    player.bowl(new Roll(5));
-    player.bowl(new Roll(5));
-    assertEquals(Score.Points.pending(), player.getPoints());
-
-    // Frame 2
-    player.bowl(new Roll(1));
-    assertEquals(Score.Points.of(11), player.getPoints());
-    player.bowl(new Roll(4));
-    assertEquals(Score.Points.of(16), player.getPoints());
-
-    player.bowl(new Roll(6));
-    player.bowl(new Roll(4));
-    player.bowl(new Roll(1));
-    assertEquals(Score.Points.of(27), player.getPoints());
-    player.bowl(new Roll(1));
-    assertEquals(Score.Points.of(29), player.getPoints());
-  }
-
-  @Test
   void totalScore() {
     //Frame 1
     player.bowl(Roll.STRIKE)
